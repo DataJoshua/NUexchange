56428b63463484e34d146a98bc661477eafaf857
Joshua Rod <rodriguez.taco.joshua@gmail.com>
2022-06-13 15:17:47 +0300

    agregado simbolo de wpp

--- js/script.js
+++ js/script.js
@@ -1,20 +1,56 @@
 $(document).ready(()=>{
-    //API para saber el valor de rublos
+    
     let valor_actual_ruble = document.querySelector(".ruble-value");
     let date = $(".date");
     let btn = $(".calculator-btn");
-    let valor_usd = $(".input-ruble");
+    let valor_usd = document.querySelector(".input-ruble");
     let valor_rub = $(".output-ruble");
 
 
+    
+    //API para saber el valor de rublos
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "8RNa1SAourpeaawdUd7WcZnncUMp85wl");
+
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+    };
+
+    /*
+
+    fetch("https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=1", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        let rate = result.result;
+
+        valor_actual_ruble.innerText = parseInt(rate);
+        return result;
+    }    
+    )
+    .catch(error => console.log('error', error)); 
+
+    */ 
+    
+    
+ 
     let ruble_icon = $(".ruble-icon");
 
+    btn.on("click", ()=>{
+
+        let result = parseInt(valor_actual_ruble.innerHTML) * parseInt(valor_usd.value); 
+   
+        valor_rub.text("Obtendras: " + result);
+
+    });
 
     ruble_icon.hide()
 
+  
 
 
-    
+
 });
 
  
